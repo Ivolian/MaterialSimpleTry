b4259cc81a84d32fed45c7432565eb59652ec4bd
renjiajian <renjiajian@withub.net.cn>
2015-06-04 13:38:27 +0800

    添加 betterspinner

--- app/src/main/java/com/ivo/materialsimpletry/fragment/EditTextFragment.java
+++ app/src/main/java/com/ivo/materialsimpletry/fragment/EditTextFragment.java
@@ -1,21 +1,49 @@
 package com.ivo.materialsimpletry.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.ivo.materialsimpletry.R;
+import com.ivo.materialsimpletry.fragment.base.BaseFragment;
+import com.rengwuxian.materialedittext.MaterialAutoCompleteTextView;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+import java.util.Arrays;
+
+import butterknife.InjectView;
 
 
-public class EditTextFragment extends Fragment {
+public class EditTextFragment extends BaseFragment {
+
+    @Override
+    public int getLayoutResourceId() {
+        return R.layout.fragment_edittext;
+    }
+
+    @InjectView(R.id.autocompletetext)
+    MaterialAutoCompleteTextView autoCompleteTextView;
+
+    @InjectView(R.id.betterspinner)
+    MaterialBetterSpinner betterSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edittext, container, false);
+
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        initViews();
+
+        return rootView;
     }
 
+    private void initViews() {
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, Arrays.asList("one", "two","three"));
+        autoCompleteTextView.setAdapter(adapter);
+        betterSpinner.setAdapter(adapter);
+    }
 
 }
